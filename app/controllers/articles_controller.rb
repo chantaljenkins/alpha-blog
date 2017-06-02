44502ac2a2102378010be01c2dd4750f42459968
Chantal Jenkins <norwegimo@icloud.com>
2017-06-01 17:30:49 -0700

    Add admin functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -54,7 +54,7 @@ class ArticlesController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @article.user
+      if current_user != @article.user and !current_user.admin?
         flash[:danger] = "You must be the owner of article to perform that action"
         redirect_to root_path
       end
